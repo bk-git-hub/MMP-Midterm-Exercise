00a8ae3002d917dd4aedc1b9a8d6b7b588318146
bk-git-hub <bkprivate99@gmail.com>
2023-04-16 01:52:15 +0900

    example 3 cut image horizontally

---
+++ ex3.cpp
@@ -0,0 +1,26 @@
+#include <opencv2/opencv.hpp>
+
+int main()
+{
+	IplImage* src = cvLoadImage("C:\\temp\\yeji.jpg");
+	CvSize size = cvGetSize(src);
+	IplImage* dst = cvCreateImage(size, 8, 3);
+	int w = size.width;
+	int h = size.height;
+	for (int y = 0; y < h; y++)
+	{
+		for (int x = 0; x < w; x++)
+		{
+			CvScalar sc = cvGet2D(src, y, x);
+			if (y < h / 2)
+				cvSet2D(dst, y + h / 2, x, sc);
+			else
+				cvSet2D(dst, y-h/2, x, sc);
+		}
+	}
+
+	cvShowImage("src", src);
+	cvShowImage("dst", dst);
+	cvWaitKey();
+	return 0;
+}
